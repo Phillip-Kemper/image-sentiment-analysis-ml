7fc5f573d26e63cae9004c0b01ab1e3bdec143c4
Phillip Kemper <phillip-kemper@gmx.net>
2020-01-03 21:29:09 +0100

    part 3 done

--- backend/sentimentAPI/imageUpload/urls.py
+++ backend/sentimentAPI/imageUpload/urls.py
@@ -3,12 +3,14 @@ from django.urls import path,include
 from . import views
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'images', views.ImageUploadViewSet)
+#router = routers.DefaultRouter()
+#router.register(r'images', views.ImageUploadList)
 
 urlpatterns = [
-    path('snippets/', views.ImageUploadList.as_view()),
-    path('snippets/<int:pk>/', views.ImageUploadDetail.as_view()),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('images/', views.ImageUploadList.as_view()),
+    path('images/<int:pk>/', views.ImageUploadDetail.as_view()),
+#    path('', include(router.urls)),
+#    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+
--- backend/sentimentAPI/imageUpload/views.py
+++ backend/sentimentAPI/imageUpload/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 
 from .serializers import ImageUploadSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import ImageUpload
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -22,52 +22,16 @@ from rest_framework.response import Response
 #def upload(self):
 #    return HttpResponse("Hello, world. You're at the polls index. 2")
 #
-class ImageUploadViewSet(viewsets.ModelViewSet):
-    queryset = ImageUpload.objects.all().order_by('name')
+#class ImageUploadViewSet(viewsets.ModelViewSet):
+#    queryset = ImageUpload.objects.all().order_by('name')
+#    serializer_class = ImageUploadSerializer
+
+
+class ImageUploadList(generics.ListCreateAPIView):
+    queryset = ImageUpload.objects.all()
     serializer_class = ImageUploadSerializer
 
 
-class ImageUploadList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request, format=None):
-        image = ImageUpload.objects.all()
-        serializer = ImageUploadSerializer(image, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ImageUploadSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ImageUploadDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return ImageUpload.objects.get(pk=pk)
-        except ImageUpload.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        image = self.get_object(pk)
-        serializer = ImageUploadSerializer(image)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ImageUploadSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ImageUploadDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ImageUpload.objects.all()
+    serializer_class = ImageUploadSerializer
