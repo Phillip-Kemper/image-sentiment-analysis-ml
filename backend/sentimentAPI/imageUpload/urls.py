22cb47cf810b0f39b84d4ded45111dd59f2b1bf0
Phillip Kemper <phillip-kemper@gmx.net>
2020-01-03 21:16:00 +0100

    class based views wip

--- backend/sentimentAPI/imageUpload/urls.py
+++ backend/sentimentAPI/imageUpload/urls.py
@@ -7,8 +7,8 @@ router = routers.DefaultRouter()
 router.register(r'images', views.ImageUploadViewSet)
 
 urlpatterns = [
-    path('snippets/', views.image_list),
-    path('snippets/<int:pk>/', views.image_detail),
+    path('snippets/', views.ImageUploadList.as_view()),
+    path('snippets/<int:pk>/', views.ImageUploadDetail.as_view()),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/sentimentAPI/imageUpload/views.py
+++ backend/sentimentAPI/imageUpload/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from rest_framework.views import APIView
+
 from .serializers import ImageUploadSerializer
 from rest_framework import viewsets
 from .models import ImageUpload
@@ -25,44 +27,47 @@ class ImageUploadViewSet(viewsets.ModelViewSet):
     serializer_class = ImageUploadSerializer
 
 
-@api_view(['GET', 'POST'])
-def image_list(request):
+class ImageUploadList(APIView):
     """
-    List all code snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         image = ImageUpload.objects.all()
         serializer = ImageUploadSerializer(image, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = ImageUploadSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def image_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        image = ImageUpload.objects.get(pk=pk)
-    except ImageUpload.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class ImageUploadDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return ImageUpload.objects.get(pk=pk)
+        except ImageUpload.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        image = self.get_object(pk)
         serializer = ImageUploadSerializer(image)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = ImageUploadSerializer(image, data=request.data)
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ImageUploadSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        image.delete()
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
