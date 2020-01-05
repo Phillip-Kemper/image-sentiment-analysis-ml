56b6275b2fd4a81a86f011bd876044d8b5e0ff05
Phillip Kemper <phillip-kemper@gmx.net>
2020-01-05 17:35:00 +0100

    transform dimensions wip

--- backend/helpers/imageTransform.py
+++ backend/helpers/imageTransform.py
@@ -11,7 +11,16 @@ def transformBlackWhite(data):
 
     return grayImage
 
-
+def transformDimensions(data):
+    originalImage = cv2.imread('/Users/phillip/Desktop/happy.jpeg')
+    width = 48
+    height = 48
+    dim = (width, height)
+    resized = cv2.resize(originalImage, dim, interpolation=cv2.INTER_AREA)
+    print('Resized Dimensions : ', resized.shape)
+    cv2.imshow("Resized image", resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 print(transformBlackWhite(''))
