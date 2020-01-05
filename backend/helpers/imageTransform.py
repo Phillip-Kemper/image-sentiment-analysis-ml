f43836bb77c9bc9838e7bfb808ddba056cdca461
Phillip Kemper <phillip-kemper@gmx.net>
2020-01-05 17:42:18 +0100

    make accessible to img as numpy array

--- backend/helpers/imageTransform.py
+++ backend/helpers/imageTransform.py
@@ -1,26 +1,27 @@
 import cv2
 
 def transformBlackWhite(data):
-    originalImage = cv2.imread('/Users/phillip/Desktop/happy.jpeg')
-    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+    grayImage = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+    return grayImage
 
-    cv2.imshow('Original image', originalImage)
-    cv2.imshow('Gray image', grayImage)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow('Original image', originalImage)
+    #cv2.imshow('Gray image', grayImage)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
     return grayImage
 
 def transformDimensions(data):
-    originalImage = cv2.imread('/Users/phillip/Desktop/happy.jpeg')
-    width = 48
-    height = 48
+    width = 48  # as needed for fer-2013 dataset
+    height = 48 #           "
     dim = (width, height)
-    resized = cv2.resize(originalImage, dim, interpolation=cv2.INTER_AREA)
-    print('Resized Dimensions : ', resized.shape)
-    cv2.imshow("Resized image", resized)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    resized = cv2.resize(data, dim, interpolation=cv2.INTER_AREA)
+    #print('Resized Dimensions : ', resized.shape)
+    #cv2.imshow("Resized image", resized)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    return resized
 
 
-print(transformBlackWhite(''))
+
+print(transformDimensions(''))
