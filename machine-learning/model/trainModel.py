49d82126de92142e13f759dfa247b4e991701443
Phillip Kemper <phillip-kemper@gmx.net>
2020-01-10 21:00:10 +0100

    data set up, X to numpy array

--- machine-learning/model/trainModel.py
+++ machine-learning/model/trainModel.py
@@ -9,12 +9,37 @@ CSV_FILE = "../dataRetrieval/sources/fer2013.csv"
 
 def main():
     trainingData, evalData = getData.load_fer(CSV_FILE)
-    print(trainingData)
+    #print(trainingData)
     y = trainingData["emotion"]
     # TODO @PK: write pixel data to X matrix
-    X = trainingData["pixels"]
+    X_data = trainingData["pixels"].values.reshape((17010, 1))
+    X = np.zeros((17010, 2304))
+
+    width, height = 48, 48
+
+    datapoints = trainingData['pixels'].tolist()
+
+    # getting features for training
+    X = []
+    for xseq in datapoints:
+        xx = [int(xp) for xp in xseq.split(' ')]
+#        xx = np.asarray(xx).reshape(1, 48*48)
+#        X.append(xx.astype('float64'))
+        X.append(xx)
+
+    X = np.asarray(X)
+    X = np.expand_dims(X, axis=0)
+    #X = np.expand_dims(X, -1)
+
+    print('X_TRAINING')
+    print(X)
+
+    # getting labels for training
+    y = pd.get_dummies(trainingData['emotion']).values
+
     return X
 
 
 res = main()
 
+#print(res)
