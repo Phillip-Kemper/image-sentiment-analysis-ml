8049a7d1d1bc4a4fe6242f2cc206536b34c5e544
Phillip Kemper <phillip-kemper@gmx.net>
2020-01-12 15:55:51 +0100

    train Model with Transposing Thetas

--- machine-learning/model/neuralNetworkBackpropagation.py
+++ machine-learning/model/neuralNetworkBackpropagation.py
@@ -70,36 +70,38 @@ def costFunction(nn_params, num_entradas, num_ocultas, num_etiquetas, X, y, reg)
     return cost
 
 
-def pesosAleatorios(L_in, L_out):
+def randomTheta(L_in, L_out):
     epsilon = 0.12
     rand = np.random.rand(L_out, L_in + 1) * 2 * epsilon - epsilon
     return rand
 
 
 def gradient(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lmbda):
-    # print("loading/training...")
+    print("loading/training...")
     initial_theta1 = np.reshape(nn_params[:hidden_layer_size * (input_layer_size + 1)],
-                                (hidden_layer_size, input_layer_size + 1), 'F')
+                                (hidden_layer_size, input_layer_size + 1), 'F').T
     initial_theta2 = np.reshape(nn_params[hidden_layer_size * (input_layer_size + 1):],
-                                (num_labels, hidden_layer_size + 1), 'F')
+                                (num_labels, hidden_layer_size + 1), 'F').T
     y_d = pd.get_dummies(y.flatten())
     delta1 = np.zeros(initial_theta1.shape)
     delta2 = np.zeros(initial_theta2.shape)
     m = len(y)
 
     for i in range(X.shape[0]):
+        print(X.shape[0])
+        print(np.shape(initial_theta1))
         ones = np.ones(1)
         a1 = np.hstack((ones, X[i]))
         z2 = np.dot(a1,initial_theta1)
         a2 = np.hstack((ones, sigmoid(z2)))
-        z3 = np.dot(a2,initial_theta2.T)
+        z3 = np.dot(a2,initial_theta2)
         a3 = sigmoid(z3)
 
         d3 = a3 - y_d.iloc[i, :][np.newaxis, :]
         z2 = np.hstack((ones, z2))
-        d2 = np.multiply(np.dot(initial_theta2.T,d3.T), (np.multiply(sigmoid(z2), 1 - sigmoid(z2))).T[:, np.newaxis])
-        delta1 = delta1 + np.dot(d2[1:, :] , a1[np.newaxis, :])
-        delta2 = delta2 + np.dot(d3.T ,a2[np.newaxis, :])
+        d2 = np.multiply(np.dot(initial_theta2,d3.T), (np.multiply(sigmoid(z2), 1 - sigmoid(z2))).T[:, np.newaxis])
+        delta1 = delta1 + np.dot(d2[1:, :] , a1[np.newaxis, :]).T
+        delta2 = delta2 + np.dot(d3.T ,a2[np.newaxis, :]).T
 
     delta1 /= m
     delta2 /= m
@@ -122,50 +124,50 @@ def predict(theta1, theta2, X, y):
     return np.argmax(h, axis=1) + 1
 
 
-data = loadmat('ex4data1.mat')
-y = data['y']
-X = data['X']
-num_etiquetas = 10
-num_entradas = 400
-num_ocultas = 25
-_lambda = 1
-epsilon = np.math.pow(10, -4)
-
-# visualize data 100 times
-for i in range(1, 10):
-    figure = plt.subplot(10, 1, i)
-    sample = np.random.choice(X.shape[0], 10)
-    plt.imshow(X[sample, :].reshape(-1, 20).T)
-    plt.axis('off')
-plt.show
-weights = loadmat('ex4weights.mat')
-
-theta1, theta2 = weights['Theta1'], weights['Theta2']
-# Theta1 es de dimension 25 x 401
-# Theta2 es de dimension 10 x 26
-thetaVector = np.append(np.ravel(theta1, order='F'), np.ravel(theta2, order='F'))
-
-# cF = costFunction(thetaVector, num_entradas, num_ocultas, num_etiquetas, X, y, _lambda)
-
-# gradientCheckResult = gradientCheck(thetaVector, num_entradas, num_ocultas, num_etiquetas, X, y, _lambda, epsilon)
-
-initialTheta1 = pesosAleatorios(num_entradas, num_ocultas)
-initialTheta2 = pesosAleatorios(num_ocultas, num_etiquetas)
-
-initialThetaVector = np.append(np.ravel(initialTheta1, order='F'), np.ravel(initialTheta2, order='F'))
-
-for i in range(11):
-    lamb = i / 10
-
-    thetaOpt = opt.fmin_cg(maxiter=70, f=costFunction, x0=initialThetaVector, fprime=onlyGrad,
-                           args=(num_entradas, num_ocultas, num_etiquetas, X, y.flatten(), lamb))
-
-    theta1 = np.reshape(thetaOpt[:num_ocultas * (num_entradas + 1)], (num_ocultas, num_entradas + 1), 'F')
-    theta2 = np.reshape(thetaOpt[num_ocultas * (num_entradas + 1):], (num_etiquetas, num_ocultas + 1), 'F')
-
-    pred = predict(theta1, theta2, X, y)
-    #    print(pred)
-    print(lamb)
-    print(np.mean(pred == y.flatten()) * 100, "%")
-
-print("end")
+#data = loadmat('ex4data1.mat')
+#y = data['y']
+#X = data['X']
+#num_etiquetas = 10
+#num_entradas = 400
+#num_ocultas = 25
+#_lambda = 1
+#epsilon = np.math.pow(10, -4)
+#
+## visualize data 100 times
+#for i in range(1, 10):
+#    figure = plt.subplot(10, 1, i)
+#    sample = np.random.choice(X.shape[0], 10)
+#    plt.imshow(X[sample, :].reshape(-1, 20).T)
+#    plt.axis('off')
+#plt.show
+#weights = loadmat('ex4weights.mat')
+#
+#theta1, theta2 = weights['Theta1'], weights['Theta2']
+## Theta1 es de dimension 25 x 401
+## Theta2 es de dimension 10 x 26
+#thetaVector = np.append(np.ravel(theta1, order='F'), np.ravel(theta2, order='F'))
+#
+## cF = costFunction(thetaVector, num_entradas, num_ocultas, num_etiquetas, X, y, _lambda)
+#
+## gradientCheckResult = gradientCheck(thetaVector, num_entradas, num_ocultas, num_etiquetas, X, y, _lambda, epsilon)
+#
+#initialTheta1 = pesosAleatorios(num_entradas, num_ocultas)
+#initialTheta2 = pesosAleatorios(num_ocultas, num_etiquetas)
+#
+#initialThetaVector = np.append(np.ravel(initialTheta1, order='F'), np.ravel(initialTheta2, order='F'))
+#
+#for i in range(11):
+#    lamb = i / 10
+#
+#    thetaOpt = opt.fmin_cg(maxiter=70, f=costFunction, x0=initialThetaVector, fprime=onlyGrad,
+#                           args=(num_entradas, num_ocultas, num_etiquetas, X, y.flatten(), lamb))
+#
+#    theta1 = np.reshape(thetaOpt[:num_ocultas * (num_entradas + 1)], (num_ocultas, num_entradas + 1), 'F')
+#    theta2 = np.reshape(thetaOpt[num_ocultas * (num_entradas + 1):], (num_etiquetas, num_ocultas + 1), 'F')
+#
+#    pred = predict(theta1, theta2, X, y)
+#    #    print(pred)
+#    print(lamb)
+#    print(np.mean(pred == y.flatten()) * 100, "%")
+#
+#print("end")
--- machine-learning/model/trainModel.py
+++ machine-learning/model/trainModel.py
@@ -4,19 +4,19 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import dataRetrieval.dataRetrieval as getData
+import model.neuralNetworkBackpropagation as ml
 
 CSV_FILE = "../dataRetrieval/sources/fer2013.csv"
 
 def main():
     trainingData, evalData = getData.load_fer(CSV_FILE)
     # print(trainingData)
-    y = trainingData["emotion"]
-    # TODO @PK: write pixel data to X matrix
-    X_data = trainingData["pixels"].values.reshape((17010, 1))
-    X = np.zeros((17010, 2304))
+    y = trainingData["emotion"].values
+    # getting labels for training
+    #y = pd.get_dummies(trainingData['emotion']).values
+
 
     width, height = 48, 48
-
     datapoints = trainingData['pixels'].tolist()
 
     # getting features for training
@@ -29,17 +29,38 @@ def main():
     # X = np.expand_dims(X, axis=0)
     # X = np.expand_dims(X, -1)
 
-    print('X_TRAINING')
-    print(X)
-    print('test')
-    print(X.shape)
-
-    # getting labels for training
-    y = pd.get_dummies(trainingData['emotion']).values
+    #print('X_TRAINING')
+    #print(X)
+    #print('test')
+    #print(X.shape)
 
     return X, y
 
 
 X, y = main()
+num_labels = 3
+num_input = 2304
+num_hidden = 25
+_lambda = 1
+epsilon = np.math.pow(10, -4)
+
+initialTheta1 = ml.randomTheta(num_input, num_hidden)
+initialTheta2 = ml.randomTheta(num_hidden, num_labels)
+
+initialThetaVector = np.append(np.ravel(initialTheta1, order='F'), np.ravel(initialTheta2, order='F'))
+
+print(np.shape(initialThetaVector))
+
+thetaOpt = opt.fmin_cg(maxiter=70, f=ml.costFunction, x0=initialThetaVector, fprime=ml.onlyGrad,
+                       args=(num_input, num_hidden, num_labels, X, y.flatten(), _lambda))
+
+theta1 = np.reshape(thetaOpt[:num_hidden * (num_input + 1)], (num_hidden, num_input + 1), 'F')
+theta2 = np.reshape(thetaOpt[num_hidden * (num_input + 1):], (num_labels, num_hidden + 1), 'F')
+
+pred = ml.predict(theta1, theta2, X, y)
+print(pred)
+print(np.mean(pred == y.flatten()) * 100, "%")
+
+
 
 #print(res)
